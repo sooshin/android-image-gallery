60cf24a5fe5cbe62a8166a45e51fda8ad120a04d
Soojeong <soojeongshin2@gmail.com>
2019-09-22 23:38:35 +0900

    feat: Use fitCenter

--- app/src/main/java/com/soojeongshin/imagegallery/BindingAdapters.kt
+++ app/src/main/java/com/soojeongshin/imagegallery/BindingAdapters.kt
@@ -15,6 +15,7 @@ fun bindImage(imgView: ImageView, imgUrl: String?) {
         val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
         Glide.with(imgView.context)
             .load(imgUri)
+            .fitCenter()
             .apply(RequestOptions()
                 .placeholder(R.drawable.loading_animation)
                 .error(R.drawable.ic_broken_image))
